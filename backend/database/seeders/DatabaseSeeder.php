2ba2de6d419527428924429a73c21a2a905b10ec
IvanCuevas-dev <csalgueroivan@gmail.com>
2026-04-30 14:18:18 +0200

    Modificado seeder para pruebas

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Character;
+use App\Models\Item;
+use App\Models\Inventory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -29,6 +31,29 @@ class DatabaseSeeder extends Seeder
             ['name' => $admin->name]
         );
 
+        $user = User::updateOrCreate(
+            ['name' => 'Usuario'],
+            [
+                'email' => 'usuario@usuario.com',
+                'password' => Hash::make('usuario1234'),
+                'role' => 'user',
+            ]
+        );
+
+        Character::updateOrCreate(
+            ['user_id' => $user->id],
+            ['name' => $user->name]
+        );
+
         $this->call(ItemSeeder::class);
+
+        $character = Character::where('user_id', $admin->id)->first();
+
+        foreach (Item::all() as $item) {
+            Inventory::updateOrCreate(
+                ['character_id' => $character->id, 'item_id' => $item->id],
+                ['quantity' => 2]
+            );
+        }
     }
 }
