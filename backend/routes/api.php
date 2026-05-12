b201d71286021d8479d99325aa5afd5fc014207d
Ivan Cuevas <csalgueroivan@gmail.com>
2026-05-12 17:47:39 +0200

    Agregado rutas panel admin

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\CharacterController;
 use App\Http\Controllers\InventoryController;
 use App\Http\Controllers\WorkController;
 use App\Http\Controllers\EquipmentController;
+use App\Http\Controllers\AdminController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -18,4 +19,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/equipment', [EquipmentController::class, 'index']);
     Route::post('/equipment/equip', [EquipmentController::class, 'equip']);
     Route::delete('/equipment/unequip/{slot}', [EquipmentController::class, 'unequip']);
+    Route::get('/admin/users', [AdminController::class, 'index']);
+    Route::post('/admin/users/{id}/ban', [AdminController::class, 'ban']);
+    Route::post('/admin/users/{id}/unban', [AdminController::class, 'unban']);
 });
